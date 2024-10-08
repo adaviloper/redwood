dfbd1802ae53a74038793cf953d30c17292ff5d1
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-10-08 18:57:43 -0500

    formatting

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,19 +1,30 @@
-import { createHead } from '@unhead/vue'
-import { createPinia } from 'pinia'
-import { createApp } from 'vue'
-import './assets/index.postcss'
-import { router } from './router'
-import App from './App.vue'
-import { plugin, defaultConfig } from '@formkit/vue'
-import formkitConfig from '../formkit.config'
+import { createHead } from '@unhead/vue';
+import { createPinia } from 'pinia';
+import { createApp } from 'vue';
+import 'primeicons/primeicons.css';
+import './assets/index.postcss';
+import { router } from './router';
+import App from './App.vue';
+import { plugin, defaultConfig } from '@formkit/vue';
+import formkitConfig from '../formkit.config';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 
-const head = createHead()
-const app = createApp(App)
+const head = createHead();
+const app = createApp(App);
 
-const pinia = createPinia()
-app.use(pinia)
-app.use(router)
-app.use(plugin, defaultConfig(formkitConfig))
-app.use(head)
+const pinia = createPinia();
+app.use(pinia);
+app.use(router);
+app.use(plugin, defaultConfig(formkitConfig));
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: false,
+    },
+  }
+});
+app.use(head);
 
-app.mount('#app')
+app.mount('#app');
