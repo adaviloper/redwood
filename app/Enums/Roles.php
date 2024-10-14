3bcde4b52bbcc3b48b13966f0fdaf6bbd33f22b3
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-10-13 21:20:27 -0500

    adding spatie permissions package

---
+++ app/Enums/Roles.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+enum Roles: string
+{
+    use HasValues;
+
+    case ADMIN = 'Admin';
+
+
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Spatie\Permission\Middleware\PermissionMiddleware;
+use Spatie\Permission\Middleware\RoleMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,6 +20,8 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->alias([
+            'permission' => PermissionMiddleware::class,
+            'role' => RoleMiddleware::class,
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
 
