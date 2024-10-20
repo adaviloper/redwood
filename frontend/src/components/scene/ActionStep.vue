843888df1d58ea7784a489c1d40bc632dc2abcdc
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-10-19 22:27:28 -0500

    moving rolling responsibility to ActionStep

--- frontend/src/components/scene/ActionStep.vue
+++ frontend/src/components/scene/ActionStep.vue
@@ -1,16 +1,54 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import type { TActionStep } from '@/types/Scenario';
+import Button from 'primevue/button';
+import type { Nullable } from '@/types/utilities';
+import { usePlayerCharacterStore } from '@/store/playerCharacter';
+import type { AbilityName } from '@/types/Ability';
 
 type Props = {
   step: TActionStep;
 };
 
 defineProps<Props>();
+const pcStore = usePlayerCharacterStore();
+
+const playerCharacter = pcStore.selectedPlayerCharacter;
+
+const modifier = (name: AbilityName) => {
+  return playerCharacter?.abilities.find(ability => ability.name === name)
+};
+
+const rollResult= ref<Nullable<number>>(null);
+
+const select = () => {
+  rollResult.value = Math.ceil(Math.random() * 20);
+};
 </script>
 
 <template>
   <div>
-    {{ step.action.type.charAt(0).toUpperCase() + step.action.type.slice(1) }} a {{ step.action.dice }} + [{{ step.action.ability.slice(0, 3).toUpperCase() }}]
+    <p>
+      {{ step.copy }}
+    </p>
+    <p>
+      {{ step.action.type.charAt(0).toUpperCase() + step.action.type.slice(1) }} a {{ step.action.dice }} + [{{ step.action.ability.slice(0, 3).toUpperCase() }}]
+    </p>
+
+      <div
+        v-if="step.type === 'step' && rollResult"
+      >
+        {{ rollResult }} + {{ modifier(step.action.ability)?.value }} = {{ rollResult + (modifier(step.action.ability)?.value || 0) }}
+      </div>
+
+      <Button
+        type="button"
+        severity="info"
+        @click="select()"
+      >
+        Select
+      </Button>
+
   </div>
 </template>
 
--- frontend/src/components/scene/OptionStep.vue
+++ frontend/src/components/scene/OptionStep.vue
@@ -1,29 +1,12 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
 import type { Step } from '@/types/Scenario';
-import Button from 'primevue/button';
-import type { Nullable } from '@/types/utilities';
-import { usePlayerCharacterStore } from '@/store/playerCharacter';
-import type { AbilityName } from '@/types/Ability';
 
 type Props = {
   step: Step;
 };
 
 defineProps<Props>();
-const pcStore = usePlayerCharacterStore();
 
-const playerCharacter = pcStore.selectedPlayerCharacter;
-
-const modifier = (name: AbilityName) => {
-  return playerCharacter?.abilities.find(ability => ability.name === name)
-};
-
-const rollResult= ref<Nullable<number>>(null);
-
-const select = () => {
-  rollResult.value = Math.ceil(Math.random() * 20);
-};
 </script>
 
 <template>
@@ -37,20 +20,6 @@ const select = () => {
         v-if="step.type === 'step'"
         :step="step"
       />
-
-      <div
-        v-if="step.type === 'step' && rollResult"
-      >
-        {{ rollResult }} + {{ modifier(step.action.ability)?.value }} = {{ rollResult + (modifier(step.action.ability)?.value || 0) }}
-      </div>
-
-      <Button
-        type="button"
-        severity="info"
-        @click="select()"
-      >
-        Select
-      </Button>
 
     </div>
   </div>
