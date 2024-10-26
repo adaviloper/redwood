50a0ca61e1eb211a323f1ab1638581779914e5d3
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-10-26 18:32:34 -0500

    creating tesing database

--- docker/docker-entrypoint-initdb.d/01-database.sql
+++ docker/docker-entrypoint-initdb.d/01-database.sql
@@ -1,6 +1,4 @@
-CREATE DATABASE IF NOT EXISTS `loop-returns` DEFAULT CHARACTER SET = `utf8mb4` DEFAULT COLLATE = `utf8mb4_unicode_ci`;
-CREATE DATABASE IF NOT EXISTS `loop-testing` DEFAULT CHARACTER SET = `utf8mb4` DEFAULT COLLATE = `utf8mb4_unicode_ci`;
-CREATE DATABASE IF NOT EXISTS `loop-commerce-data` DEFAULT CHARACTER SET = `utf8mb4` DEFAULT COLLATE = `utf8mb4_unicode_ci`;
-CREATE DATABASE IF NOT EXISTS `loop-commerce-data-testing` DEFAULT CHARACTER SET = `utf8mb4` DEFAULT COLLATE = `utf8mb4_unicode_ci`;
+CREATE DATABASE IF NOT EXISTS `redwood` DEFAULT CHARACTER SET = `utf8mb4` DEFAULT COLLATE = `utf8mb4_unicode_ci`;
+CREATE DATABASE IF NOT EXISTS `redwood_testing` DEFAULT CHARACTER SET = `utf8mb4` DEFAULT COLLATE = `utf8mb4_unicode_ci`;
 
 GRANT ALL ON *.* TO 'laravel'@'%';
