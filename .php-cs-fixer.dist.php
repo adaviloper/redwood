e43de55d2bb49e2790623dba662222ac2b3a1e49
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-10-27 21:33:16 -0500

    new lint fixes

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,12 +2,21 @@
 
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__)
+    ->exclude([
+        'docker',
+        'frontend',
+        'public',
+        'resources',
+        'storage',
+        'vendor',
+    ])
 ;
 
 return (new PhpCsFixer\Config())
     ->setRules([
         '@PER-CS' => true,
         '@PHP83Migration' => true,
+        'fully_qualified_strict_types' => true,
     ])
     ->setFinder($finder)
 ;
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,13 +16,13 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
 
         $middleware->alias([
             'permission' => PermissionMiddleware::class,
             'role' => RoleMiddleware::class,
-            'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
+            'verified' => App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
 
         $middleware->statefulApi();
