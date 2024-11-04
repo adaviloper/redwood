cde171b0aac4fa99906c5ffbf41769bff5dcfb07
adaviloper <16654653+adaviloper@users.noreply.github.com>
2024-11-03 19:51:04 -0600

    removing unused package

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,8 +5,6 @@ import 'primeicons/primeicons.css';
 import './assets/index.postcss';
 import { router } from './router';
 import App from './App.vue';
-import 'clockwork-browser/metrics';
-import 'clockwork-browser/toolbar';
 import { defaultConfig, plugin } from '@formkit/vue';
 import formkitConfig from '../formkit.config';
 import PrimeVue from 'primevue/config';
