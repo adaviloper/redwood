3f612d429cbcb0c14b4ff8aef7aaaa769f4e91ca
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-03-29 20:35:55 -0500

    adding phpunit group

--- tests/Unit/Models/CharacterTest.php
+++ tests/Unit/Models/CharacterTest.php
@@ -4,8 +4,10 @@ namespace Tests\Unit\Models;
 
 use App\Enums\CharacterClasses;
 use App\Models\Character;
+use PHPUnit\Framework\Attributes\Group;
 use Tests\TestCase;
 
+#[Group('character')]
 class CharacterTest extends TestCase
 {
     public function testCharacterClassGetsCastToEnum(): void
