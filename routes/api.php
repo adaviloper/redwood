d8169c2f1d1b9fa6da0d7d4af83cc85ff3924cd7
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-12-24 12:16:07 -0600

    adding test route

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,10 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('users', function() {
+    return User::all();
+});
+
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('characters', CharacterController::class);
     Route::apiResource('player-characters', PlayerCharacterController::class);
